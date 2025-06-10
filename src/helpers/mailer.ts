15c26414c0a8260daab38bf9847842f3aac835b2
Mayank Yadav <97794801+mayank05y@users.noreply.github.com>
2025-06-10 14:53:09 +0530

    Error Corrected

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -23,7 +23,7 @@ export const sendEmail = async ({email, emailType, userId}:any) => {
                 forgotPasswordTokenExpiry: Date.now() + 3600000})
         }
     
-        let transport = nodemailer.createTransport({
+        const transport = nodemailer.createTransport({
             host: "sandbox.smtp.mailtrap.io",
             port: 2525,
             auth: {
